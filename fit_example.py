5ef2d2d4d544a47a3c3f6284cb49daeb02b232b0
Ho Fung Tsoi <hftsoi0411@gmail.com>
2024-10-27 21:43:35 -0400

    add fit_example.py

---
+++ fit_example.py
@@ -0,0 +1,31 @@
+from symbolfit.symbolfit import *
+
+dataset = importlib.import_module('examples.datasets.toy_dataset_1.dataset')
+pysr_config = importlib.import_module('examples.pysr_configs.pysr_config_1')
+
+model = SymbolFit(
+    x = dataset.x,
+    y = dataset.y,
+    y_up = dataset.y_up,
+    y_down = dataset.y_down,
+    pysr_config = pysr_config,
+    max_complexity = 60,
+    input_rescale = True,
+    scale_y_by = 'mean',
+    max_stderr = 40,
+    fit_y_unc = True,
+    random_seed = 42,
+    loss_weights = None
+    )
+                  
+model.fit()
+
+model.save_to_csv(output_dir = 'output_dir/')
+
+model.plot_to_pdf(
+    output_dir = 'output_dir/',
+    bin_widths_1d = dataset.bin_widths_1d,
+    #bin_edges_2d = dataset.bin_edges_2d,
+    plot_logy = False,
+    plot_logx = False
+    )
