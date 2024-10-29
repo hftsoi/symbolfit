50fa8522509c31dc478235237deed0fa362a4fb0
Ho Fung Tsoi <hftsoi0411@gmail.com>
2024-10-28 22:02:57 -0400

    clean setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
 from setuptools import setup
-from setuptools.command.install import install
-import subprocess
 
-class custom_install(install):
-    def run(self):
-        install.run(self)
-        subprocess.check_call(["python3", "-m", "pysr", "install"])
-
-setup(
-    setup_requires=['pysr==0.16.9'],
-    cmdclass={
-        'install': custom_install
-    }
-)
+if __name__ == "__main__":
+    setup()
