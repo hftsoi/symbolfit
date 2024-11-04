e797872761ea231998c398fee14262e3c06a33b6
Ho Fung Tsoi <hftsoi0411@gmail.com>
2024-11-04 12:04:38 -0500

    add sampling_95quantile option to sampling plots
     Date:      Mon Nov 4 12:07:38 2024 -0500

--- fit_example.py
+++ fit_example.py
@@ -1,7 +1,7 @@
 from symbolfit.symbolfit import *
 
-dataset = importlib.import_module('examples.datasets.dijet.dataset')
-pysr_config = importlib.import_module('examples.pysr_configs.pysr_config_2').pysr_config
+dataset = importlib.import_module('examples.datasets.toy_dataset_1.dataset')
+pysr_config = importlib.import_module('examples.pysr_configs.pysr_config_1').pysr_config
 
 model = SymbolFit(
     x = dataset.x,
@@ -9,9 +9,9 @@ model = SymbolFit(
     y_up = dataset.y_up,
     y_down = dataset.y_down,
     pysr_config = pysr_config,
-    max_complexity = 80,
+    max_complexity = 60,
     input_rescale = True,
-    scale_y_by = None,
+    scale_y_by = 'mean',
     max_stderr = 20,
     fit_y_unc = True,
     random_seed = None,
@@ -26,6 +26,7 @@ model.plot_to_pdf(
     output_dir = 'output_dir/',
     bin_widths_1d = dataset.bin_widths_1d,
     #bin_edges_2d = dataset.bin_edges_2d,
-    plot_logy = True,
-    plot_logx = True
+    plot_logy = False,
+    plot_logx = False,
+    sampling_95quantile = False
 )
