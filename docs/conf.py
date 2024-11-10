e87eb21c7211cb85a2cdcb1dce17f7b54930a5cc
Ho Fung Tsoi <hftsoi0411@gmail.com>
2024-11-10 13:58:24 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,8 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+html_static_path = ['_static']
+
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
