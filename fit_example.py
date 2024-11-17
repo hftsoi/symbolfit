6dc1e9c2891b18ac5b945589634d898915eb3e01
Ho Fung Tsoi <hftsoi0411@gmail.com>
2024-11-17 00:26:38 -0500

    update

--- fit_example.py
+++ fit_example.py
@@ -25,8 +25,15 @@ model.save_to_csv(output_dir = 'output_dir/')
 model.plot_to_pdf(
     output_dir = 'output_dir/',
     bin_widths_1d = dataset.bin_widths_1d,
-    #bin_edges_2d = dataset.bin_edges_2d,
     plot_logy = False,
     plot_logx = False,
-    sampling_95quantile = False
+    sampling_95quantile = False,
+    #bin_edges_2d = dataset.bin_edges_2d,
+    #plot_logx0 = False,
+    #plot_logx1 = False,
+    #cbar_min = None,
+    #cbar_max = None,
+    #cmap = None,
+    #contour = None,
+    # ^ additional options for 2D plotting
 )
